9740f51fcb87341839818f873e3dce08c09b6085
Mario Prieschl <priemar.dev@gmail.com>
2019-03-28 17:05:15 +0100

    added (mat-autocomplete) example for using pri-scrollbar in mat-autocomplete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {PriScrollbarModule} from 'pri-ng-scrollbar';
+import {MatAutocompleteModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     BrowserModule,
-    PriScrollbarModule
+    PriScrollbarModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   declarations: [
     AppComponent,
